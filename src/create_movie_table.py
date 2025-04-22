a545ba5fe69ae9d05ed2599e6c55748ce3e78d16
Emanuele Fabbiani <donlelef@gmail.com>
2025-04-22 12:19:20 +0200

    feat: add Langfuse integration and update movie table creation

--- src/create_movie_table.py
+++ src/create_movie_table.py
@@ -8,7 +8,10 @@ from src.paths import DATA_PATH, LANCEDB_URI
 
 
 def main():
-    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s", level=logging.INFO)
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        level=logging.INFO,
+    )
 
     logging.info(f"Reading data from {DATA_PATH}...")
     df = pl.read_parquet(DATA_PATH)
@@ -16,17 +19,21 @@ def main():
     db = lancedb.connect(LANCEDB_URI)
 
     logging.info("Creating table...")
-    table = db.create_table("words", schema=Movie, mode="overwrite")
+    table = db.create_table("movie", schema=Movie, mode="overwrite")
     logging.info("Adding data to table...")
     batch_size = 2_000
     for i in range(0, df.height, batch_size):
         logging.info(f"Adding rows {i} to {i + batch_size}...")
-        table.add(df[i:i + batch_size], mode='append')
+        table.add(df[i : i + batch_size], mode="append")
 
     logging.info("Running query...")
-    res: Movie = table.search("I would like a movie with dragons.").limit(1).to_pydantic(Movie)[0]
+    res: Movie = (
+        table.search("I would like a movie with dragons.")
+        .limit(1)
+        .to_pydantic(Movie)[0]
+    )
     logging.info(f"Query results: \n Title: {res.title} \n Overview: {res.overview}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/run_rag_query.py
+++ src/run_rag_query.py
@@ -1,9 +1,122 @@
 import logging
+import uuid
+
+import lancedb
+from langfuse.decorators import observe, langfuse_context
+from langfuse.openai import OpenAI
+
+from src.embeddings import Movie
+from src.paths import LANCEDB_URI
 
 
+@observe
+def build_hyde_query(client: OpenAI, query: str) -> str:
+    prompt = f"""
+    Consider the following query, related to a movie: {query}
+    Create a one-sentence summary for a movie relevant to the query.
+    You do not need to describe an existing movie, feel free to invent, but stay relevant to the query.
+    """
+    response = client.chat.completions.create(
+        model="gpt-4.1",
+        messages=[
+            {"role": "user", "content": prompt},
+        ],
+    )
+    return response.choices[0].message.content
+
+
+def run_semantic_query(
+    db: lancedb.DBConnection, query: str, limit: int = 3
+) -> list[Movie]:
+    res: list[Movie] = (
+        db.open_table("movie").search(query).limit(limit).to_pydantic(Movie)
+    )
+    return res
+
+
+def format_movies(movies: list[Movie]) -> str:
+    return "\n".join(
+        [
+            f"**{movie.title}** - Genre: {movie.genre} - Release: {movie.release_date} \n {movie.overview}"
+            for movie in movies
+        ]
+    )
+
+
+@observe
+def run_reranking(
+    client: OpenAI,
+    movies: list[Movie],
+    query: str,
+) -> str:
+    formated_movies = format_movies(movies)
+    prompt = f"""
+    Consider the following query, related to a movie: {query}
+    The following movies were proposed as relevant to the query:
+    {formated_movies}
+    Please re-rank the movies based on their relevance to the query, removing any duplicate and any irrelevant item.
+    Only return the updated list, with all the information for each movie.
+    """
+    response = client.chat.completions.create(
+        model="gpt-4.1",
+        messages=[
+            {"role": "user", "content": prompt},
+        ],
+    )
+    return response.choices[0].message.content
+
+
+@observe
+def answer_query_from_context(client: OpenAI, context: str, query: str) -> str:
+    prompt = f"""
+    Consider the following query, related to a movie: {query}
+    The following movies were proposed as relevant to the query:
+    {context}
+    Provide an answer to the query, choosing the most relevant movie, in a friendly and open tone.
+    """
+    response = client.chat.completions.create(
+        model="gpt-4.1",
+        messages=[
+            {"role": "user", "content": prompt},
+        ],
+    )
+    return response.choices[0].message.content
+
+
+@observe
 def main():
-    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s", level=logging.INFO)
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        level=logging.INFO,
+    )
+    client = OpenAI()
+    query = "I would like to watch a movie with dragons"
+    session_id = uuid.uuid4().hex
+    langfuse_context.update_current_trace(session_id=session_id)
+    logging.info(f"Session ID: {session_id}")
+
+    logging.info(f"Query: {query}")
+    logging.info("Building hyde query...")
+    hyde_query = build_hyde_query(client, query)
+    logging.info(f"Hyde query: {hyde_query}...")
+
+    logging.info("Connecting to LanceDB...")
+    db = lancedb.connect(LANCEDB_URI)
+    logging.info("Running queries...")
+    res: list[Movie] = run_semantic_query(db, query)
+    hyde_res: list[Movie] = run_semantic_query(db, hyde_query)
+    logging.info(
+        f"Length for original query {len(res)}, length of hyde query: {len(hyde_res)}"
+    )
+
+    logging.info("Re-ranking results...")
+    reranked_movies = run_reranking(client, res + hyde_res, query)
+    logging.info(f"Reranked movies: {reranked_movies}")
+
+    logging.info("Answering query...")
+    answer = answer_query_from_context(client, reranked_movies, query)
+    logging.info(f"Answer: {answer}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
