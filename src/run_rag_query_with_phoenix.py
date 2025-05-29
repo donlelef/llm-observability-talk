d8ce48353f6f048f9b09fc46b9bfd1d09e836789
Emanuele Fabbiani <donlelef@gmail.com>
2025-05-29 12:20:51 +0200

    feat: integrate arize-phoenix for feedback annotations and refactor query handling

--- src/run_rag_query_with_phoenix.py
+++ src/run_rag_query_with_phoenix.py
@@ -1,36 +1,17 @@
 import logging
-import os
 import uuid
 
-import httpx
 import lancedb
 from openai import OpenAI
 from openinference.instrumentation import using_session, using_user
 from opentelemetry.trace import format_span_id, get_current_span, StatusCode
+from phoenix.client import Client
 from phoenix.otel import register
 
 from embeddings import Movie
 from paths import LANCEDB_URI
 from rag import build_hyde_query, run_semantic_query, run_reranking, answer_query_from_context
 from scores import ThumbScore
-
-
-def set_score(name: str, score: ThumbScore):
-    httpx.post(
-        f"{os.getenv('PHOENIX_COLLECTOR_ENDPOINT')}/v1/span_annotations?sync=false",
-        json={
-            "data": [
-                {
-                    "span_id": format_span_id(get_current_span().get_span_context().span_id),
-                    "name": name,
-                    "annotator_kind": "HUMAN",
-                    "result": {"label": score.name, "score": score.value},
-                    "metadata": {},
-                }
-            ]
-        },
-        headers={"api_key": os.getenv("PHOENIX_API_KEY")},
-    )
 
 
 def main():
@@ -48,17 +29,19 @@ def main():
     user_id = "AnonimizedLele"
     logging.info(f"Session ID: {session_id}, user ID: {user_id}")
 
+    openai_client = OpenAI()
+    phoenix_client = Client()
+
     with (
         tracer.start_as_current_span("run_rag_query_with_phoenix", openinference_span_kind="chain") as span,
         using_session(session_id),
         using_user(user_id),
     ):
-        client = OpenAI()
         query = "I would like to watch a movie with dragons"
 
         logging.info(f"Query: {query}")
         logging.info("Building hyde query...")
-        hyde_query = build_hyde_query(client, query)
+        hyde_query = build_hyde_query(openai_client, query)
         logging.info(f"Hyde query: {hyde_query}...")
 
         logging.info("Connecting to LanceDB...")
@@ -69,15 +52,21 @@ def main():
         logging.info(f"Length for original query {len(res)}, length of hyde query: {len(hyde_res)}")
 
         logging.info("Re-ranking results...")
-        reranked_movies = run_reranking(client, res + hyde_res, query)
+        reranked_movies = run_reranking(openai_client, res + hyde_res, query)
         logging.info(f"Reranked movies: {reranked_movies}")
 
         logging.info("Answering query...")
-        answer = answer_query_from_context(client, reranked_movies, query)
+        answer = answer_query_from_context(openai_client, reranked_movies, query)
         logging.info(f"Answer: {answer}")
 
         logging.info("Sending feedback to answer...")
-        set_score("user_thumbs", ThumbScore.THUMB_UP)
+        phoenix_client.annotations.add_span_annotation(
+            span_id=format_span_id(get_current_span().get_span_context().span_id),
+            annotation_name="feedback",
+            annotator_kind="HUMAN",
+            label=ThumbScore.THUMB_UP.name,
+            score=ThumbScore.THUMB_UP.value,
+        )
         span.set_input(query)
         span.set_output(answer)
         span.set_status(StatusCode.OK)
